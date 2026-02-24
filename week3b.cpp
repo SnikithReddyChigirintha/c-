8211ee0668f7781e6bac85c40758a12c9321b274
Snikith Reddy <snikithreddychigirintha@gmail.com>
2026-02-24 14:49:07 +0530

    Week-3 Program Completed

---
+++ week3b.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+    list<string>l;
+    l.push_back("C++");
+    l.push_back("Java");
+    l.push_back("Python");
+    cout<<"Initial list: \n";
+    for(list<string>::iterator it=l.begin(); it!=l.end(); it++){
+        cout<<*it<<" ";
+    }
+    l.push_front("DS");
+    l.pop_back();
+    cout<<"\n\nAfter isertion and deletion: \n";
+    for(list<string>::iterator it=l.begin(); it!=l.end(); it++){
+        cout<<*it<<" "; 
+    }
+    return 0;
+}
